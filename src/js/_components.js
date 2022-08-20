5ba5bac5ce121a5f84425bc344a6748fada6db4b
andrew <andreyrekagg@gmail.com>
2022-08-20 12:29:15 +0300

    added purhcases page

--- src/js/_components.js
+++ src/js/_components.js
@@ -140,26 +140,28 @@ const swiper = new Swiper(".swiper-stock-preview", {
   });
 
 //Show more (news section)
-const showMoreBtn = document.getElementById('showMore');
+const showMoreBtns = document.querySelectorAll('.news-section__btn--show-more');
 const showMoreNews = document.querySelectorAll('.news-section__item--more');
 const newsSectionList = document.querySelector('.news-section__list--page');
 
-showMoreBtn.addEventListener('click', () => {
-    showMoreBtn.classList.add('hidden');
-    showMoreNews.forEach(item => {
-        item.classList.toggle('hidden');
-    });
-    const showLessBtn = document.createElement('button');
-    showLessBtn.classList.add('btn', 'btn__accent', 'btn-reset', 'btn__wide', 'news-section__btn');
-    showLessBtn.textContent = 'Показать меньше';
-    newsSectionList.append(showLessBtn);
-
-    showLessBtn.addEventListener('click', () => {
-        showLessBtn.remove();
+showMoreBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.add('hidden');
         showMoreNews.forEach(item => {
             item.classList.toggle('hidden');
         });
-        showMoreBtn.classList.remove('hidden');
+        const showLessBtn = document.createElement('button');
+    
+        showLessBtn.classList.add('btn', 'btn__accent', 'btn-reset', 'btn__wide', 'news-section__btn');
+        showLessBtn.textContent = 'Показать меньше';
+        newsSectionList.append(showLessBtn);
+    
+        showLessBtn.addEventListener('click', () => {
+            showLessBtn.remove();
+            showMoreNews.forEach(item => {
+                item.classList.toggle('hidden');
+            });
+            btn.classList.remove('hidden');
+        });
     });
 });
-console.log(showMoreNews);
